df1c5752ac06ec43c67f3da4a8503f24dd0a5f8e
Fodan <7767514+fodan@user.noreply.gitee.com>
2020-07-28 23:40:42 +0800

    initial commits

---
+++ ExplodeEverything/ExplodeEverythingComponent.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using ExplodeEverything.Properties;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Parameters;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using Rhino.Render.Fields;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace ExplodeEverything
+{
+    public class ExplodeEverythingComponent : GH_Component, IGH_VariableParameterComponent
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+
+        string typeName;
+        FieldInfo[] fieldsArr;
+        PropertyInfo[] propertiesArr;
+
+        public ExplodeEverythingComponent()
+          : base("ExplodeEverything", "EE",
+              "Description",
+              "Math", "Playground")
+        {
+        }
+
+        protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
+        {
+            base.AppendAdditionalComponentMenuItems(menu);
+            Menu_AppendItem(menu, "Match object Fields", MatchResponder);
+        }
+
+        void MatchResponder(object o, EventArgs e)
+        {
+            if (fieldsArr == null || propertiesArr == null)
+                return;
+
+            if (this.Params.Output.Count == fieldsArr.Length + propertiesArr.Length)
+                return;
+
+            while (this.Params.Output.Count < fieldsArr.Length + propertiesArr.Length)
+            {
+                this.Params.RegisterOutputParam(new Param_GenericObject());
+            }
+            while (this.Params.Output.Count > fieldsArr.Length + propertiesArr.Length)
+            {
+                this.Params.UnregisterOutputParameter(this.Params.Output[this.Params.Output.Count - 1]);
+            }
+
+            this.Params.OnParametersChanged();
+            this.VariableParameterMaintenance();
+            this.ExpireSolution(true);
+        }
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Object", "O", "Object to explode", GH_ParamAccess.item);
+            pManager[0].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("", "", "", GH_ParamAccess.tree);
+            this.VariableParameterMaintenance();
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //DA.DisableGapLogic();
+            object obj = new object();
+            if (!DA.GetData(0, ref obj))
+                return;
+
+            if (DA.Iteration < 1)
+            {
+                typeName = obj.GetType().Name;
+            }
+            else
+            {
+                if (obj.GetType().Name != typeName)
+                {
+                    throw new Exception("Only same type of object can be explode");
+                }
+            }
+
+            Type t = obj.GetType();
+            if (t.Name.StartsWith("GH_"))
+            {
+                try
+                {
+                    GH_ObjectWrapper wrapper = (GH_ObjectWrapper)obj;
+                    t = wrapper.Value.GetType();
+                    obj = wrapper.Value;
+                }
+                catch
+                {
+                    obj = obj.GetType()
+                        .GetProperty("Value", BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy)
+                        .GetValue(obj);
+                    t = obj.GetType();
+                }
+            }
+            fieldsArr = t.GetFields(BindingFlags.GetField | BindingFlags.Instance | BindingFlags.Public);
+            propertiesArr = t.GetProperties(BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.Public);
+
+            for (int ind = 0; ind < fieldsArr.Length + propertiesArr.Length; ++ind)
+            {
+                if (ind >= fieldsArr.Length + propertiesArr.Length)
+                {
+                    this.Params.Output[ind].NickName = "?";
+                }
+                else
+                {
+                    if (ind < fieldsArr.Length)
+                    {
+                        try
+                        {
+                            Params.Output[ind].NickName = fieldsArr[ind].Name;
+                            DA.SetData(ind, fieldsArr[ind].GetValue(obj));
+                        }
+                        catch
+                        {
+                            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Some fields were failed to explode, check answer carefully");
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            Params.Output[ind].NickName = propertiesArr[ind - fieldsArr.Length].Name;
+                            DA.SetData(ind, propertiesArr[ind - fieldsArr.Length].GetValue(obj));
+                        }
+                        catch
+                        {
+                            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Some fields were failed to explode, check answer carefully");
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public bool CanInsertParameter(GH_ParameterSide side, int index)
+        {
+            return side == GH_ParameterSide.Output;
+        }
+
+        public bool CanRemoveParameter(GH_ParameterSide side, int index)
+        {
+            return side == GH_ParameterSide.Output;
+        }
+
+        public IGH_Param CreateParameter(GH_ParameterSide side, int index)
+        {
+            return new Param_GenericObject();
+        }
+
+        public bool DestroyParameter(GH_ParameterSide side, int index)
+        {
+            return true;
+        }
+
+        public void VariableParameterMaintenance()
+        {
+            if (fieldsArr != null && propertiesArr != null)
+            {
+                if (this.Params.Output.Count > 0)
+                {
+                    for (int ind = 0; ind < fieldsArr.Length + propertiesArr.Length; ++ind)
+                    {
+                        if (ind < fieldsArr.Length)
+                        {
+                            Params.Output[ind].NickName = fieldsArr[ind].Name;
+                        }
+                        else
+                        {
+                            Params.Output[ind].NickName = propertiesArr[ind - fieldsArr.Length].Name;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void DrawViewportMeshes(IGH_PreviewArgs args)
+        {
+            return;
+        }
+
+        public override void DrawViewportWires(IGH_PreviewArgs args)
+        {
+            return;
+        }
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return Resources.iconfinder_Funny_132217;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b62ce553-c5bf-4d00-a76e-84b8d5fbb17b"); }
+        }
+    }
+}
---
+++ ExplodeEverything/ExplodeEverythingInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using ExplodeEverything.Properties;
+using Grasshopper.Kernel;
+
+namespace ExplodeEverything
+{
+    public class ExplodeEverythingInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "ExplodeEverything";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return Resources.iconfinder_Funny_132217;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("fa409005-b969-4b96-b0d6-ababa85e2f00");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
